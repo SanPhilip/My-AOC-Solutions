3198e6431f071c8ca67416b6381f78fc7e0dc359
sanphilip <vsans.fs@gmail.com>
2024-02-08 16:27:44 -0500

    Day 3
    Check for numbers with symbols located to the left and right

--- 2023/day3.c
+++ 2023/day3.c
@@ -13,21 +13,38 @@ int main()
         return 1;
     }
 
-    int num = 0, col = 0;
+    int num = 0, col = 0, sol = 0;
     long curpos = ftell(data), prevline = ftell(data);
     bool symbol = false;
     char c;
     while((c = fgetc(data)) != EOF)
     {
+        ++col;
         if(c >= '0' && c <= '9')
         {
-            num = 0;
             do
-            {
+             {
                 num = num*10 + (c-48);
                 c = fgetc(data);
+                ++col;
             }while (c >= '0' && c <= '9');
+        }
+
+        if(symbol && num == 0) symbol = false;
+
+        if(c != '.' && c != '\n')
+        {
+            symbol = true;
+            if(num == 0) continue;
+        }
+
+        if(symbol)
+        {
+            sol += num;
+            symbol = false;
             printf("%d\n", num);
         }
+
+        num = 0;
     }
 }
