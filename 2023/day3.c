54dffd59a549297b897ecb7b55d89b87c2fc4fad
sanphilip <vsans.fs@gmail.com>
2024-02-10 14:33:52 -0500

    Day 3
    Implemented check_pline function

--- 2023/day3.c
+++ 2023/day3.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+int count_ndigits(int num)
+{
+    int ndigits = 0;
+    do
+    {
+        ++ndigits;
+        num /= 10;
+    } while(num != 0);
+    return ndigits;
+}
+
+bool check_pline(FILE *data, long prevline, int col, int ndigits)
+{
+    fseek(data, prevline, SEEK_SET);
+    char c;
+
+    for(int i = 0; i < col; ++i)
+    {
+        c = fgetc(data);
+        if(i >= col-(ndigits+2))
+            if((c != '.' && c != '\n') && !(c >= '0' && c <= '9'))
+                return true;
+    }
+    return false;
+}
+
 int main()
 {
     const char *fname = "input.txt";
@@ -13,8 +39,8 @@ int main()
         return 1;
     }
 
-    int num = 0, col = 0, sol = 0;
-    long curpos = ftell(data), prevline = ftell(data);
+    int num = 0, col = 0, sol = 0, ndigits = 0;
+    long curpos = 0, prevline = 0, curline = 0;
     bool symbol = false;
     char c;
     while((c = fgetc(data)) != EOF)
@@ -28,15 +54,28 @@ int main()
                 c = fgetc(data);
                 ++col;
             }while (c >= '0' && c <= '9');
+            ndigits = count_ndigits(num);
         }
 
-        if(symbol && num == 0) symbol = false;
-
-        if(c != '.' && c != '\n')
+        if(c == '\n')
         {
-            symbol = true;
-            if(num == 0) continue;
+            prevline = curline;
+            curline = ftell(data);
+            col = 0;
         }
+
+        /* if(symbol && num == 0) symbol = false; */
+
+        /* if(c != '.' && c != '\n') */
+        /* { */
+        /*     symbol = true; */
+        /*     if(num == 0) continue; */
+        /* } */
+
+        curpos = ftell(data);
+
+        if(num != 0)
+            symbol = check_pline(data, prevline, col, ndigits);
 
         if(symbol)
         {
@@ -44,6 +83,8 @@ int main()
             symbol = false;
             printf("%d\n", num);
         }
+
+        fseek(data, curpos, SEEK_SET);
 
         num = 0;
     }
